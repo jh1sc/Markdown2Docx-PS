f15945b3e54390cdba5c480cc26138ae261e932a
Jh1sc <105714284+jh1sc@users.noreply.github.com>
2023-02-05 18:59:30 +0800

    Add files via upload

---
+++ Md2Docx-PS.ps1
@@ -0,0 +1,39 @@
+
+if ([bool](winget install pandoc) -ne $true) { winget install pandoc }
+Function FOSD {
+    [CmdletBinding()]
+    Param(
+        [string]$Description = "Select a folder",
+        [string]$RootFolder = "Desktop"
+    )
+    Add-Type -AssemblyName System.Windows.Forms
+    $FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $FolderBrowserDialog.Description = $Description
+    $FolderBrowserDialog.RootFolder = $RootFolder
+    $FolderBrowserDialog.ShowDialog() | Out-Null
+    return $FolderBrowserDialog.SelectedPath
+}
+Function FISD {
+    Param(
+        [string]$Title = "Select a file",
+        [string[]]$Filter = @("All Files (*.*)|*.*")
+    )
+    Add-Type -AssemblyName System.Windows.Forms
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.Title = $Title
+    $OpenFileDialog.Filter = ($Filter -join "|")
+    $OpenFileDialog.ShowDialog() | Out-Null
+    return $OpenFileDialog.FileName
+}
+
+$MarkDown = FISD -Title "Choose a .md file" -Filter @("Markdown Files (*.md)|*.md")
+$mdC = Get-Content -Path $MarkDown
+$StartIndex = $mdC.IndexOf("## ASSISTANT") + 1
+$TrimmedContent = $mdC[$StartIndex..$mdC.Length]
+Set-Content -Path $MarkDown -Value $TrimmedContent
+$Directory = FOSD -Description "Select a directory to create the new docx in"
+$Directory = $Directory+"\MarkDown.docx"
+pandoc -s $MarkDown -o $Directory --reference-doc .\ref.docx
+Start-Process $Directory 
+
+
